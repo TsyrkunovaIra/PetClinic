5e81b5ff3675fa07d97f527d6dc3b404589130b1
TsyrkunovaIra <157691083+TsyrkunovaIra@users.noreply.github.com>
2024-06-25 08:59:59 +0300

    History

---
+++ src/main/java/com/home/project/model/clinic/HistoryClient.java
@@ -0,0 +1,48 @@
+package com.home.project.model.clinic;
+
+public class HistoryClient {
+    public class HistoryClient {
+        public class HistoryClient {
+            private ArrayList<Animal> animals;
+            private String animalName;
+            private String dataVisit;
+            private String nameIll;
+            private String treatment;
+            private double score;
+            public HistoryClient (String dataVisit, String nameIll, String treatment, double score){
+
+                this.dataVisit = dataVisit;
+                this.nameIll = nameIll;
+                this.treatment = treatment;
+                this.score = score;
+            }
+            public String getDataVisit() {return dataVisit;}
+            public String getNameIll() {return nameIll;}
+            public String getTreatment() {return treatment;}
+            public double getScore() {return score;}
+            public void setDataVisit(String dataVisit) {this.dataVisit = dataVisit;}
+            public void setNameIll(String nameIll) {this.nameIll = nameIll;}
+            public void setTreatment(String treatment) {this.treatment = treatment;}
+            public void setScore(double score) {this.score = score;}
+            public String toString(){
+                return "History visit client: " + dataVisit + ", " + nameIll + ", " + treatment + ", " + score + "$";}
+
+            public ArrayList<Animal> getAnimals() {
+                return animals;
+            }
+
+            public void setAnimals(ArrayList<Animal> animals) {
+                this.animals = animals;
+            }
+
+            public String getAnimalName() {
+                return animalName;
+            }
+
+            public void setAnimalName(String animalName) {
+                this.animalName = animalName;
+            }
+        }
+    }
+
+}
