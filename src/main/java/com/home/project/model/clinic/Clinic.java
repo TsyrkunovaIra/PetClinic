79f7fe40af4260cdf9f2db6a811e8f9b6c5c17dc
TsyrkunovaIra <157691083+TsyrkunovaIra@users.noreply.github.com>
2024-06-25 16:32:34 +0300

    boolean

--- src/main/java/com/home/project/model/clinic/Clinic.java
+++ src/main/java/com/home/project/model/clinic/Clinic.java
@@ -1,11 +1,11 @@
 package com.home.project.model.clinic;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-
-
+import java.util.ArrayList;
 
 public class Clinic {
-    private boolean Work;
+    private ArrayList<Client> basa;
+    private long id;
     public DayOfWeek getDayOfWeek(){
         return LocalDateTime.now().getDayOfWeek();}
     public void setDayOfWeek(DayOfWeek dayOfWeek){}
@@ -15,11 +15,19 @@ public class Clinic {
             {
                 Schedule day = values[i];
                 if(LocalDateTime.now().getDayOfWeek().name().equals(day.name()))
-                    System.out.println();
+                    System.out.println();}}}
 
-            }
-        }
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public ArrayList<Client> getBasa() {return basa;}
+    public void setBasa(ArrayList<Client> basa) {this.basa = basa;}
+
+    @Override
+    public boolean equals(Object client) {
+        return super.equals(client);
+        if (this == client) return true;
+        if ((client == null || getClass() != client.getClass())) return false;
+        Client that = (Client) client;}
 
     }
 
-}
