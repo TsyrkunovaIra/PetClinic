d03b4d6eb205f864d9d524c8c027ee937600de1f
TsyrkunovaIra <157691083+TsyrkunovaIra@users.noreply.github.com>
2024-09-04 22:29:56 +0300

    registrator

--- src/main/java/com/home/project/model/clinic/Client.java
+++ src/main/java/com/home/project/model/clinic/Client.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Client extends Person {
     private ArrayList<Animal> animals;
-    private ArrayList<HistoryClient> historyVisit;
+    private ArrayList<HistoryClient> historyVisits;
     private Animal animal;
     public ArrayList<Animal> getAnimals() {return animals;}
     public Client (String name, String address, int age)
@@ -13,14 +13,14 @@ public class Client extends Person {
     public void setAnimals(ArrayList<Animal> animals) {this.animals = animals;}
     public void setPrintAnimalName(String printAnimalName){}
 
-    public ArrayList <HistoryClient> historyVisits() {return historyVisit;}
-    public void setHistoryVisit(ArrayList<HistoryClient> historyVisit) {this.historyVisit = historyVisit;}
+    public ArrayList <HistoryClient> historyVisits() {return historyVisits;}
+    public void setHistoryVisit(ArrayList<HistoryClient> historyVisits) {this.historyVisits = historyVisits;}
     public Animal getAnimal() {return animal;}
     public void setAnimal(Animal animal) {this.animal = animal;}
     public void getPrintAnimalName() {
         for (Animal aN : animals) { System.out.println(aN.getAnimalName());}}
     public String toString(){
-        return "Client: " + getName() + ", " + getAge()+ ", " + getAddress() + getAnimals() + historyVisit;}
+        return "Client: " + getName() + ", " + getAge()+ ", " + getAddress() + getAnimals() + historyVisits;}
     @Override
     public boolean equals(Object p) { return super.equals(p);}
     @Override
