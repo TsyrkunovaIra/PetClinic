c011ce14a55b437d37e3dc3a62de20f7c4d23e47
TsyrkunovaIra <157691083+TsyrkunovaIra@users.noreply.github.com>
2024-09-04 21:55:04 +0300

    redact client

--- src/main/java/com/home/project/model/clinic/Client.java
+++ src/main/java/com/home/project/model/clinic/Client.java
@@ -4,40 +4,29 @@ import com.home.project.model.animals.Animal;
 import java.util.ArrayList;
 
 public class Client extends Person {
-
     private ArrayList<Animal> animals;
     private ArrayList<HistoryClient> historyVisit;
+    private Animal animal;
     public ArrayList<Animal> getAnimals() {return animals;}
     public Client (String name, String address, int age)
     {super (name, address, age);}
     public void setAnimals(ArrayList<Animal> animals) {this.animals = animals;}
     public void setPrintAnimalName(String printAnimalName){}
+
+    public ArrayList <HistoryClient> historyVisits() {return historyVisit;}
+    public void setHistoryVisit(ArrayList<HistoryClient> historyVisit) {this.historyVisit = historyVisit;}
+    public Animal getAnimal() {return animal;}
+    public void setAnimal(Animal animal) {this.animal = animal;}
     public void getPrintAnimalName() {
         for (Animal aN : animals) { System.out.println(aN.getAnimalName());}}
-    public ArrayList <HistoryClient> historyVisits() {
-        return historyVisit;}
-    public void setHistoryVisit(ArrayList<HistoryClient> historyVisit) {
-        this.historyVisit = historyVisit;}
-
     public String toString(){
         return "Client: " + getName() + ", " + getAge()+ ", " + getAddress() + getAnimals() + historyVisit;}
     @Override
-    public boolean equals(Object c) {
-        if (this == c) return true;
-        if ((c == null || getClass() != c.getClass())) return false;
-        Client that = (Client) c;
-        if (getName() != that.getName()) return false;
-        if (getAge() != that.getAge()) return false;
-        if (getAddress() != that.getAddress()) return false;
-        return getName().equals(that.getName());}
+    public boolean equals(Object p) { return super.equals(p);}
     @Override
     public int hashCode() {
-        int result = getName() == null ? 0 : getName().hashCode();
-        int result1 = getAddress() == null ? 0 : getAddress().hashCode();
-        result = result + result1 + getAge();
-        return result;}
-    private static void card(Object aObject){
-        System.out.println(String.valueOf(aObject));}}
+        return super.hashCode();}
+}
 
 
 
